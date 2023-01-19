b065cfbae0a44349143bf6c7b2712cebcc2928ba
Fylkirist <112450556+Fylkirist@users.noreply.github.com>
2023-01-19 10:47:03 +0100

    oppdatering av mockdata og script

---
+++ script.js
@@ -0,0 +1,31 @@
+fetch("./Mockdata/cars.json")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+    renderListItems(data)
+    return data
+})
+.catch(error => {
+    console.log(error)});
+
+const shopList = document.getElementById('annonseliste')
+
+var storageArray = [];
+
+function renderListItems(carInfo)
+{
+    for(i=0;i<carInfo.length;i++){
+        console.log(carInfo[i])
+        let shopListElement = document.createElement("div")
+        shopListElement.className="listElement"
+        shopListElement.innerHTML+=`<img class="bilBilde" src="${carInfo[i]["img"]}"/>`
+        shopListElement.innerHTML+=`<div class="bilNavn">${carInfo[i]["model"]}</div>`
+        shopListElement.innerHTML+=`<div class="bilPris">${carInfo[i]["price"]}</div>`
+        shopListElement.innerHTML+=`<div class="bilTilstand">${carInfo[i]["condition"]}</div>`
+        shopListElement.innerHTML+=`<div class="handlevognEmptyIcon">
+        <i class="bi bi-cart-plus" class="handlevognPlus"></i></div>`
+        shopList.appendChild(shopListElement)
+    }
+};
